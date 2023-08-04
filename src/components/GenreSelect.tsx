6a21fcbbb788deababdf36e41bfa0393efc1cdaf
Zichao Zang <zang.code@gmail.com>
2023-08-04 12:39:02 +0100

    add linting script to test changes FINAL

--- src/components/GenreSelect.tsx
+++ src/components/GenreSelect.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 // this file currently uses 2 spaces for indentation!
+// adding a comment so I can commit this file.
 const GenreSelect = (props: Props) => {
-  const testfn = () => "double quoted string without semicolon at end"
-  const longFunction = () => { const test = '1234'; const why = 'hello hello hello '; const hello = "how are you doing?" }
+    const testfn = () => 'double quoted string without semicolon at end';
+    const longFunction = () => {
+        const test = '1234';
+        const why = 'hello hello hello ';
+        const hello = 'how are you doing?';
+    };
 
-  const trailingCommas = {
-    commaAfterThisKey: 1,
-    missingCommaAfterThisKey: 2
-  }
-  const crampedObject = {hi: 1, goodbye: 2}
+    const trailingCommas = {
+        commaAfterThisKey: 1,
+        missingCommaAfterThisKey: 2,
+    };
+    const crampedObject = { hi: 1, goodbye: 2 };
 
-  return (
-    <div>GenreSelect
+    return <div>GenreSelect</div>;
+};
 
-    </div>
-  )
-}
-
-export default GenreSelect
+export default GenreSelect;
