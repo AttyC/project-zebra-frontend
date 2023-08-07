dbc79565f0a204dcd24432c698fe87e16349ea4d
Brian <brian@vvgonline.com>
2023-08-07 11:09:11 +0100

    add comment for future dev

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -22,13 +22,18 @@ const PersonDetails: React.FC = () => {
     };
 
     fetchPersonData();
-  }, []);
+  }, [API_URL]);
 
   return (
     <div>
       {personData ? (
         <>
           <h2>{personData.name}</h2>
+          {personData.profile_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w200${personData.profile_path}`} //BW - Move this to the server and just send profile_path, make sure to do url encoding
+            />
+          )}
           <p>Birthday: {personData.birthday}</p>
           <p>Place of Birth: {personData.place_of_birth}</p>
           <p>Department: {personData.known_for_department}</p>
