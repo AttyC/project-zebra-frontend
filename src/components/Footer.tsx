53937e79344e55d0f8841d8b9a63c03dccd44e3c
Atty Cronin <antoniajcronin@gmail.com>
2023-08-08 16:29:04 +0100

    Remove hightlight for delselected genres

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,7 @@
 export const Footer: React.FC = () => {
-  return (
-    <footer className="bg-light">
-      <div className="logo">Project Zebra</div>
-      <nav>
-        <ul className="nav">
-          <li className="list">nav link</li>
-        </ul>
-      </nav>
-      <div>Created by Team B</div>
-    </footer> 
-  )
-}
+    return (
+        <footer className="bg-light">
+            <div>Created by Team B</div>
+        </footer>
+    );
+};
