aca6f65dae5c4d382fc82b21a8555bdee3b72424
Zichao Zang <zang.code@gmail.com>
2023-08-08 13:34:54 +0100

    Remove /test endpoint

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,45 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 import Movie from './Movie';
 import { IMovie } from '../types/interfaces';
 
 const Home = () => {
-  const [movies, setMovies] = useState<IMovie[]>([]);
+    const [movies, setMovies] = useState<IMovie[]>([]);
 
-  useEffect(() => {
-    const API_URL = `http://localhost:3000/test`;
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch(API_URL);
-        console.log('res :', response);
+    useEffect(() => {
+        const API_URL = `http://localhost:3000/movies`;
+        const fetchMovies = async () => {
+            try {
+                const response = await fetch(API_URL);
+                console.log('res :', response);
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log('Data : ', data);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                console.log('Data : ', data);
 
-        setMovies(data.results);
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-        return [];
-      }
-    };
-    fetchMovies();
-  }, []);
+                setMovies(data);
+            } catch (error) {
+                console.error('Error fetching movies:', error);
+                return [];
+            }
+        };
+        fetchMovies();
+    }, []);
 
-  return (
-      <section>
-          <h1>Popular Movies</h1>
-          <ul className="flex flex-wrap justify-between">
-              {movies?.map((movie) => (
-                  <li key={movie.id}>
-                      <Movie movie={movie} />
-                  </li>
-              ))}
-          </ul>
-      </section>
-  );
+    return (
+        <div className="main">
+            <h1>Popular Movies</h1>
+            <ul>{movies?.map((movie) => <Movie movie={movie} />)}</ul>
+        </div>
+    );
 };
 
-export default Home
+export default Home;
