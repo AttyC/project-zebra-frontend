f695ba721b0f9899d452e5f1c915f9546d9f2d43
Atty Cronin <antoniajcronin@gmail.com>
2023-08-09 19:21:45 +0100

    Add Navigation including health to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom';
+
 export const Footer: React.FC = () => {
     return (
         <footer className="bg-orange-700 py-8 text-white font-semibold text-center">
             <p>Created by Team B - Tech Returners 2023</p>
+            <nav className="py-3 flex justify-center">
+                <ul className="flex space-x-6  text-md">
+                    <li>
+                        <Link to="/" className="nav-link text-white">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/upcomingMovies"
+                            className="nav-link text-white"
+                        >
+                            Upcoming
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/genres" className="nav-link text-white">
+                            Genres
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/health" className="nav-link text-white">
+                            API Health
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </footer>
     );
 };
