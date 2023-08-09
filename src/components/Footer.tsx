024c3ab9b26c72785fe8482e6281606962a0ba42
Atty Cronin <antoniajcronin@gmail.com>
2023-08-09 20:03:10 +0100

    Style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import Nav from './Nav';
 
 export const Footer: React.FC = () => {
     return (
-        <footer className="bg-orange-700 py-8 text-white font-semibold text-center">
-            <p>Created by Team B - Tech Returners 2023</p>
+        <footer className="bg-orange-700 py-8 text-white font-semibold flex flex-col items-center">
+            <p className="pb-4">Created by Team B - Tech Returners 2023</p>
             <Nav />
         </footer>
     );
